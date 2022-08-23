cdc2ce73e2ab82e22bd7516302511c43ddace883
bsse1017kavi <bsse1017@iit.du.ac.bd>
2022-08-23 21:15:29 +0600

    Initial Commit

---
+++ whiletest.py
@@ -0,0 +1,19 @@
+a = 100
+b = 30
+print(a+b)
+
+if a+b > 150:
+	print("hi")
+
+elif a<100:
+	print("Hello")
+
+else:
+	print("No")
+
+# a= 10
+# while(a <= 0):
+# 	if a == 5:
+# 		print(a)
+# 	a += 1
+# print("exited")
