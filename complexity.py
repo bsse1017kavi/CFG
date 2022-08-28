7e3daee738edbb18e47f62e4c5cb520b654494e7
bsse1017kavi <bsse1017@iit.du.ac.bd>
2022-08-29 01:42:49 +0600

    Trying for multiple functions

--- complexity.py
+++ complexity.py
@@ -1,13 +1,23 @@
 import ast
 
+flag = False
 count = 0
+current_func = ''
 
 with open('whiletest.py') as f:
     tree = ast.parse(f.read())  # parse the file
 
 for node in ast.walk(tree):  # walk the tree
+    if isinstance(node, ast.FunctionDef):
+        if not flag:
+            current_func = str(node.name)
+            flag = True
+        else:
+            print("Cyclometric Complexity for function " + current_func + " is: " + str(count+1))
+            current_func = str(node.name)
+            count = 0      
+
     if isinstance(node, ast.If) or isinstance(node, ast.While) or isinstance(node, ast.For):  # if the node is an If (or an If-else) 
         count+=1
 
-complexity = count+1
-print(complexity)
+print("Cyclometric Complexity for function " + current_func + " is: " + str(count+1))
--- whiletest.py
+++ whiletest.py
@@ -1,25 +1,50 @@
-a = 100
-b = 30
-print(a+b)
+# a = 100
+# b = 30
+# print(a+b)
 
-if a+b > 150:
-	if a==b:
-		print("a==b")
+# if a+b > 150:
+# 	if a==b:
+# 		print("a==b")
+# 	else:
+# 		print("a!=b")
+
+# elif a<100:
+# 	print("Hello")
+
+# else:
+# 	print("No")
+
+# z()
+
+# # a= 10
+# # while(a <= 0):
+# # 	if a == 5:
+# # 		print(a)
+# # 	a += 1
+# # print("exited")
+
+# for i in range(5):
+# 	print("hello")
+
+def z():
+	print('Hi')
+
+def x():
+	a = 100
+	b = 30
+	print(a+b)
+
+	if a+b > 150:
+		if a==b:
+			print("a==b")
+		else:
+			print("a!=b")
+
+	elif a<100:
+		print("Hello")
+
 	else:
-		print("a!=b")
+		print("No")
 
-elif a<100:
-	print("Hello")
-
-else:
-	print("No")
-
-# a= 10
-# while(a <= 0):
-# 	if a == 5:
-# 		print(a)
-# 	a += 1
-# print("exited")
-
-for i in range(5):
-	print("hello")
+def hello():
+	print("hello world")
